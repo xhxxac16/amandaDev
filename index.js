4ee9bdb196166956b509e62846deb52e5a96f76f
Xia Haixia <xiahaixia@chexiang.com>
2017-03-30 11:24:39 +0800

    更新证书

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+#!/usr/bin/env node
 
-console.log("amandadev 运行ing")
+/*
+     MIT License http://www.opensource.org/licenses/mit-license.php
+     Author  @xhxxac16
+ */
+ var path = require('path');
+
+ console.log("amandadev");
+
+ var arguments = process.argv.splice(2);
+
+ console.log("arguments:", arguments);
